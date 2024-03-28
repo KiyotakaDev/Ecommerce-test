c5abf8ec39536e02095778f25a5749db090c30fb
KiyotakaDev <c4mo.sw@gmail.com>
2024-03-27 21:58:02 -0500

    feat(routes): Adding products/new route

--- client/src/routes/App.routes.jsx
+++ client/src/routes/App.routes.jsx
@@ -1,22 +1,25 @@
-import { Route, Routes } from 'react-router-dom'
-import AdminLayout from '../components/AdminLayout'
+import { Route, Routes } from "react-router-dom";
+import AdminLayout from "../components/AdminLayout";
+import Products from "../pages/admin/Products";
+import NewProduct from "../pages/admin/NewProduct";
 
 const AppRoutes = () => {
   return (
     <Routes>
       {/* Norma */}
-      <Route path='/' >
+      <Route path="/">
         <Route index element={<div>Home</div>} />
       </Route>
 
       {/* Admin */}
-      <Route path='/admin/*' element={<AdminLayout />} >
-        <Route path='products' element={<div>Producst</div>} />
-        <Route path='orders' element={<div>Orders</div>} />
-        <Route path='settings' element={<div>Settings</div>} />
+      <Route path="/admin/*" element={<AdminLayout />}>
+        <Route path="products" element={<Products />} />
+        <Route path="products/new" element={<NewProduct />} />
+        <Route path="orders" element={<div>Orders</div>} />
+        <Route path="settings" element={<div>Settings</div>} />
       </Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
