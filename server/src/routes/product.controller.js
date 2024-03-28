b2006c7ce993ac0fcc37d5a31aef2a4b89a0b50a
KiyotakaDev <c4mo.sw@gmail.com>
2024-03-28 17:39:18 -0500

    feat(patch): Adding route

--- server/src/routes/product.controller.js
+++ server/src/routes/product.controller.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { createProduct, getAllProducts } from '../controllers/product.controller.js'
+import { createProduct, getAllProducts, updateProduct } from '../controllers/product.controller.js'
 
 const router = Router()
 
 router.post('/products', createProduct)
 router.get('/products', getAllProducts)
+router.patch('/product/:id', updateProduct)
 
 export default router
