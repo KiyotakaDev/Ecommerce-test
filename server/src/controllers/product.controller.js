c18eaae5fd2fb41c23bc9f91802385c8f954cea9
KiyotakaDev <c4mo.sw@gmail.com>
2024-03-28 13:53:17 -0500

    feat(createProduct): Controller is working correctly

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -1,7 +1,19 @@
+import { pool } from '../utils/database.js'
+
 export const createProduct = async (req, res) => {
   try {
-    console.log(req.body);
-    res.sendStatus(200)
+    const { title, description, price: pr } = req.body;
+    const price = parseInt(pr)
+    const client = await pool.connect()
+    const query = `
+      INSERT INTO products (title, description, price)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+    `;
+    const values = [title, description, price]
+    const queryResult = await client.query(query, values);
+    client.release()
+    return res.status(204).json({ product: queryResult.rows[0] })
   } catch (error) {
     return res.status(500).json({ error: "Current error: " + error })
   }
