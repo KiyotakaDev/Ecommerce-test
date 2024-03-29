3ad8a198a5cd2bfd62b29ccbbe8af7f416579207
KiyotakaDev <c4mo.sw@gmail.com>
2024-03-29 14:25:32 -0500

    feat(Edit): Edit page is created

--- client/src/pages/admin/Edit.jsx
+++ client/src/pages/admin/Edit.jsx
@@ -1,22 +1,87 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Edit = () => {
-  const { id } = useParams()
-  
-  useEffect(() => {
-    const editProduct = async () => {
-      const response = await axios.patch(`http://localhost:3000/api/product/${id}`)
-    }
+  const navigate = useNavigate()
+  const { id } = useParams();
+  const [product, setProduct] = useState({
+    title: "",
+    description: "",
+    price: "",
+  });
 
-    editProduct()
-  }, [id])
-  
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/api/product/${id}`
+        );
+        setProduct({
+          title: response.data.title,
+          description: response.data.description,
+          price: response.data.price,
+        });
+      } catch (error) {
+        // Toast error
+        toast.error("Edit product error");
+      }
+    };
+
+    getProduct();
+  }, [id]);
+
+  const handleSubmit = async (e) => {
+    const { title, description, price } = product;
+    const data = { title, description, price };
+
+    e.preventDefault();
+    try {
+      await axios.put(
+        `http://localhost:3000/api/product/${id}`,
+        data
+      );
+      navigate("/admin/products")
+    } catch (error) {
+      toast.error("Sendig form error");
+    }
+  };
 
   return (
-    <div>Product</div>
-  )
-}
+    <>
+      {/* Form container */}
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <h1 className="text-teal-900 font-bold my-2 mb-4 text-xl">
+          Edit product
+        </h1>
+        <label>Product name</label>
+        <input
+          type="text"
+          value={product.title}
+          placeholder="Product Name"
+          onChange={(e) => setProduct({...product, title: e.target.value})}
+        />
+        <label>Description</label>
+        <textarea
+          value={product.description}
+          placeholder="description"
+          onChange={(e) => setProduct({...product, description: e.target.value})}
+        />
+        <label>Price</label>
+        <input
+          type="text"
+          value={product.price}
+          placeholder="price"
+          onChange={(e) => setProduct({...product, price: e.target.value})}
+        />
+        <button className="btn-primary">Save</button>
+      </form>
+      {/* Toast container */}
+      <ToastContainer autoClose={2500} />
+    </>
+  );
+};
 
-export default Edit
+export default Edit;
