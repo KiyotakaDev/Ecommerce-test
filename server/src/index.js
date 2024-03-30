107580b40bc16466911c271e2199046a6b4d25de
KiyotakaDev <c4mo.sw@gmail.com>
2024-03-30 16:44:19 -0500

    style(prettier)

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import cors from "cors";
 
-import ProductRoutes from './routes/product.controller.js'
+import ProductRoutes from "./routes/product.controller.js";
 
 const app = express();
 
 // Middelwares
-app.use(cors({
-  origin: "http://localhost:5173"
-}));
-app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+app.use(express.json()); // Body JSON
 
-app.use("/api", ProductRoutes)
+app.use("/api", ProductRoutes);
 
 app.listen(3000, () => console.log(">> Server on port 3000 <<"));
