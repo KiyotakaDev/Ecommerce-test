b4f786d0e2012fb79220affef0a6ab3d1d6151f8
KiyotakaDev <c4mo.sw@gmail.com>
2024-03-30 16:44:58 -0500

    del(createProduct): Controller logic is deleted

--- server/src/controllers/product.controller.js
+++ server/src/controllers/product.controller.js
@@ -3,18 +3,6 @@ import { pool } from "../utils/database.js";
 export const createProduct = async (req, res) => {
   try {
     console.log(req.body);
-    // const { title, description, price: pr } = req.body;
-    // const price = parseInt(pr);
-    // const client = await pool.connect();
-    // const query = `
-    //   INSERT INTO products (title, description, price)
-    //   VALUES ($1, $2, $3)
-    //   RETURNING *;
-    // `;
-    // const values = [title, description, price];
-    // const queryResult = await client.query(query, values);
-    // res.json(queryResult.rows[0]);
-    // client.release();
   } catch (error) {
     return res.status(500).json({ error: "Current error: " + error });
   }
